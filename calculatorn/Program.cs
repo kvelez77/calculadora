207d0007e423b77506a05071d2fff96662bbcbb6
kvelez77 <132024818+kvelez77@users.noreply.github.com>
2023-05-08 20:32:09 -0500

    guardar numeros

--- calculatorn/Program.cs
+++ calculatorn/Program.cs
@@ -10,16 +10,35 @@ namespace calculatorn
             int num1 = 0;
             int num2 = 0;
             int result = 0;
-            
+
             Console.WriteLine("Bienvenido a la calculadora");
             Console.WriteLine("Ingresa la cantidad de numeros que deseas calcular");
             int cannumero = int.Parse(Console.ReadLine());
 
             for (int i = 1; i <= cannumero; i++)
             {
-                Console.Write("Ingresa el numero {i}: ");
+                Console.Write($"Ingresa el número {i}: ");
                 string input = Console.ReadLine();
+
+                // Validar que solo se ingresen números
+                if (!int.TryParse(input, out int num))
+                {
+                    Console.WriteLine("Error: debes ingresar un número.");
+                    i--;
+                    continue;
+                }
+
+                // Guardar los números ingresados en las variables correspondientes
+                if (i == 1)
+                {
+                    num1 = num;
+                }
+                else
+                {
+                    num2 = num;
+                }
             }
+
 
         }
     }
