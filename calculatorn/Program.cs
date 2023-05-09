86ec14cc3c277533b09afacb8eb1b463087439fd
kvelez77 <132024818+kvelez77@users.noreply.github.com>
2023-05-08 20:15:54 -0500

    validar cantidad numeros

--- calculatorn/Program.cs
+++ calculatorn/Program.cs
@@ -6,7 +6,21 @@ namespace calculatorn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int cho = 0;
+            int num1 = 0;
+            int num2 = 0;
+            int result = 0;
+            
+            Console.WriteLine("Bienvenido a la calculadora");
+            Console.WriteLine("Ingresa la cantidad de numeros que deseas calcular");
+            int cannumero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= cannumero; i++)
+            {
+                Console.Write("Ingresa el numero {i}: ");
+                string input = Console.ReadLine();
+            }
+
         }
     }
 }
